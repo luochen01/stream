b3c2bf040c80ea60abd8a106a38a1cf5ff7b63bd
Gift <ps9tg@virginia.edu>
2017-05-22 15:02:52 -0700

    fix groupby and add initialize,reset

--- src/main/java/edu/uci/asterix/stream/execution/operators/GroupbyOperator.java
+++ src/main/java/edu/uci/asterix/stream/execution/operators/GroupbyOperator.java
@@ -8,7 +8,6 @@ import java.util.Map;
 import edu.uci.asterix.stream.execution.Tuple;
 import edu.uci.asterix.stream.expr.Expr;
 import edu.uci.asterix.stream.expr.aggr.AggregateExpr;
-import edu.uci.asterix.stream.field.StructType;
 import edu.uci.asterix.stream.logical.LogicalGroupby;
 import edu.uci.asterix.stream.utils.Utils;
 
@@ -19,65 +18,32 @@ public class GroupbyOperator extends UnaryOperator<LogicalGroupby> {
     //e.g., count(*) as count
     protected final List<AggregateExpr> aggregateExprs;
 
-    private final Iterator<Object[]> iterator;
+    private Iterator<Object[]> iterator;
 
-    private final Map<Object[], Tuple> groups = new HashMap<>();
+    private Map<Object[], Object[]> groups = new HashMap<>();
 
     public GroupbyOperator(Operator child, LogicalGroupby logicalGroupby) {
         super(child, logicalGroupby);
         this.byFields = logicalGroupby.getByFields();
         this.aggregateExprs = logicalGroupby.getAggregateExprs();
 
-        StructType currentSchema = logicalGroupby.getSchema();
-        StructType childSchema = child.getSchema();
-        Object[] groupValues = new Object[currentSchema.getFields().size()];
-        Object[] key = new Object[byFields.size()];
-
-        Tuple tuple;
-
-        while ((tuple = child.next()) != null) {
-
-            //Get key using byFields
-
-            for (Expr expr : byFields) {
-
-                String byFieldName = expr.toField().getFieldName();
-                int byFieldPosition = currentSchema.getFieldIndex(byFieldName);
-                groupValues[byFieldPosition] = tuple.get(childSchema.getFieldIndex(byFieldName));
-
-                key[byFieldPosition] = expr.eval(tuple);
-
-            }
-
-            //Get aggregate values
-            Tuple groupKey = groups.get(key);
-
-            if (groupKey != null) {
-                groupValues = groupKey.getAllValues();
-                for (AggregateExpr agg : aggregateExprs) {
-                    int currentPosition = logicalGroupby.getSchema().getFieldIndex(agg.toField().getFieldName());
-                    Object currentValue = groupValues[currentPosition];
-                    groupValues[currentPosition] = agg.compute(key, currentValue, tuple);
-                }
-            } else {
-                for (AggregateExpr agg : aggregateExprs) {
-                    int currentPosition = logicalGroupby.getSchema().getFieldIndex(agg.toField().getFieldName());
-                    groupValues[currentPosition] = agg.compute(key, null, tuple);
-                }
-            }
-
-            Tuple newTuple = new Tuple(currentSchema, groupValues);
-            groups.put(key, newTuple);
-
-        }
-        iterator = groups.keySet().iterator();
-
     }
 
     @Override
     public Tuple next() {
         if (iterator.hasNext()) {
-            return groups.get(iterator.next());
+            Object[] byFields = iterator.next();
+            Object[] aggValues = groups.get(byFields);
+
+            Object[] mergedValues = new Object[byFields.length+aggValues.length];
+            int index = 0;
+            for (Object value : byFields) {
+                mergedValues[index++] = value;
+            }
+            for (Object value : aggValues) {
+                mergedValues[index++] = value;
+            }
+            return new Tuple(getSchema(), mergedValues);
         }
         return null;
     }
@@ -90,13 +56,54 @@ public class GroupbyOperator extends UnaryOperator<LogicalGroupby> {
     @Override
     public void initialize() {
         super.initialize();
-        //TODO implement initialize for gorup by
+
+        Object[] key = new Object[byFields.size()];
+        Object[] aggValues = new Object[aggregateExprs.size()];
+
+        Tuple tuple;
+
+        while ((tuple = child.next()) != null) {
+
+            //Get key using byFields
+
+            int index = 0;
+            for (Expr expr : byFields) {
+                key[index++] = expr.eval(tuple);
+            }
+
+            //Get aggregate values
+            Object[] groupValues = groups.get(key);
+
+            if (groupValues != null) {
+                aggValues = groupValues;
+                int currentPosition = 0;
+                for (AggregateExpr agg : aggregateExprs) {
+                    Object currentValue = aggValues[currentPosition];
+                    aggValues[currentPosition] = agg.compute(key, currentValue, tuple);
+                    currentPosition++;
+                }
+            } else {
+                int currentPosition = 0;
+                for (AggregateExpr agg : aggregateExprs) {
+                    aggValues[currentPosition++] = agg.compute(key, null, tuple);
+                }
+            }
+
+            groups.put(key, aggValues);
+
+        }
+        iterator = groups.keySet().iterator();
     }
 
     @Override
     public void reset() {
         super.reset();
-        //TODO implement reset for group by
+
+        if (groups != null) {
+            groups.clear();
+            groups = null;
+        }
+        iterator = null;
     }
 
     @Override
